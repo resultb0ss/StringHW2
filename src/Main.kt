28e89c5ff6ed0557a3948809572603fe82d7476f
Admin <reteamboss@gmail.com>
2024-09-07 12:10:13 +0300

    Задание 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
 fun main() {
-    println("Hello World!")
+    var phone = "+79293405657"
+    var sev = "+7"
+    var numberLength = phone.substring(2,phone.length)
+
+    if (phone.substring(0,2) == sev && numberLength.length == 10){
+        println("Номер введен верно true")
+    } else {
+        println("Номер введен неверно false")
+    }
+
 }
